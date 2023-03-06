83bb0eeacf7cfdd05e66eab305cd84d74a22dd69
MozartOnWeb <mozartontheweb@gmail.com>
2023-03-06 00:42:04 +0000

    working on the navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,24 +5,53 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav>
-      <div className="logo">Mandjou Dama.</div>
+      <Link href={""} className="logo">
+        Mandjou Dama.
+      </Link>
 
-      <div className="links">
-        <ul>
-          <li>Services</li>
-          <li>Travaux</li>
-          <li>Blog</li>
-          <li>Let&apos;s talk</li>
-        </ul>
+      <div className="linksWrapper">
+        <div className="links">
+          <Link href={""}>Services</Link>
+          <Link href={""}>Travaux</Link>
+          <Link href={""}>Blog</Link>
+          <Link className="lets-talk" href={""}>
+            <span className="one">
+              <span>L</span>
+              <span>e</span>
+              <span>t</span>
+              <span>&apos;</span>
+              <span>s</span>
+              <span> </span>
+              <span>t</span>
+              <span>a</span>
+              <span>l</span>
+              <span>k</span>
+            </span>
+            <span className="two">
+              <span>L</span>
+              <span>e</span>
+              <span>t</span>
+              <span>&apos;</span>
+              <span>s</span>
+              <span> </span>
+              <span>t</span>
+              <span>a</span>
+              <span>l</span>
+              <span>k</span>
+            </span>
+          </Link>
+        </div>
+
+        <div className="vertical-bar" />
+
+        <div className="socials">
+          <Link href={""}>GH</Link>
+          <Link href={""}>LI</Link>
+          <Link href={""}>TW</Link>
+        </div>
       </div>
 
-      <div className="bar"></div>
-
-      <div className="socials">
-        <Link href={""}>GH</Link>
-        <Link href={""}>LI</Link>
-        <Link href={""}>TW</Link>
-      </div>
+      <div className="horizontal-bar" />
     </nav>
   );
 }
