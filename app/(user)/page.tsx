b854663d40dd0c6b2685078bc9cbbd406fb31624
MozartOnWeb <mozartontheweb@gmail.com>
2023-03-06 16:36:04 +0000

    hero section finish

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,3 +1,31 @@
+import { HeroIcon } from "@/public/assets/icons";
+
 export default function Home() {
-  return <main>Welcome Home</main>;
+  return (
+    <main className="home">
+      <section className="hero">
+        <div className="info-container">
+          <div>
+            <h1 className="headline">
+              I&apos;m Mandjou, a 24 years old Front-End developer from Mali.
+            </h1>
+            <p className="subline">
+              Are you looking for perfect website ? Then you are in the right
+              place. get professional results you are looking for. Your work
+              will be done on time and as you require. just reach out and let me
+              know your needs.
+            </p>
+          </div>
+          <HeroIcon />
+        </div>
+        <div className="video-container">
+          <video
+            autoPlay={true}
+            src={require("../../public/assets/videos/hero.mp4")}
+          ></video>
+        </div>
+        <span className="horizontal-bar" />
+      </section>
+    </main>
+  );
 }
