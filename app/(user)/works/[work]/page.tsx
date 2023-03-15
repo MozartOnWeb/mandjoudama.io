f3cdf76b7c4e39243743a1fee43d50a31ff4a6db
MozartOnWeb <mozartontheweb@gmail.com>
2023-03-15 18:59:59 +0000

    setting up single work page

--- app/(user)/works/[work]/page.tsx
+++ app/(user)/works/[work]/page.tsx
@@ -1,3 +1,50 @@
+import { VerticalSeparator } from "@/components/separators/Separators";
+
 export default function Work() {
-  return <main className="single-work-page"></main>;
+  return (
+    <main className="single-work-page">
+      <section className="hero">
+        <div className="left">
+          <h1>DANCING STARS</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet
+            sit amet cursus sit amet dictum sit amet justo. Dui nunc mattis enim
+            ut tellus elementum. Cum sociis natoque penatibus et magnis dis. In
+            mollis nunc sed id semper risus.
+          </p>
+        </div>
+
+        {/* SEPARATOR */}
+        <VerticalSeparator />
+
+        <div className="right">
+          <div className="work-detail">
+            <p>Client</p>
+            <p>Tiecoura N&apos;Daou</p>
+          </div>
+          <div className="work-detail">
+            <p>Year</p>
+            <p>2021</p>
+          </div>
+          <div className="work-detail">
+            <p>TIMELINE</p>
+            <p>8 weeks</p>
+          </div>
+          <div className="work-detail">
+            <p>Services</p>
+            <div className="work-detail-services">
+              <p>Web dev</p>
+              <p>web design</p>
+              <p>content</p>
+            </div>
+          </div>
+          <div className="work-detail">
+            <p>Website</p>
+            <p>tiecourandaou.com</p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
