cdad1199c71ef41d3d73d28080f762b80b20c89a
MozartOnWeb <mozartontheweb@gmail.com>
2023-03-18 13:11:39 +0000

    setting up contact page

--- app/(user)/contact/page.tsx
+++ app/(user)/contact/page.tsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+
 export default function Contact() {
   return (
     <main className="contact-page">
-      <h1>Contact</h1>
+      <div className="contact-page-hero">
+        <div className="left">
+          <h1>Let&apos;s Connect and Create Something Great Together.</h1>
+
+          <div>
+            <h5>Find me on</h5>
+            <div className="socials-links">
+              <Link href={""}>GH</Link>
+              <Link href={""}>LI</Link>
+              <Link href={""}>TW</Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="right">
+          <form method="post">
+            <input type="text" />
+            <input type="email" />
+            <textarea cols={30} rows={10} />
+          </form>
+        </div>
+      </div>
     </main>
   );
 }
